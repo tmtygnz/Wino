623934f548c1ac1b7f8bc09d3858aa9315c94025
!!Timothy <jostimian@gmail.com>
2024-03-17 12:03:28 +0800

    impl of da two

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include "state_handler.hpp"
-#include
+#include "welay.hpp"
+
+int phase = ANIKAPhases::setup;
 
 void setup() {
     Wire.begin(8);
+
     Serial.begin(115200);
     while (!Serial);
     Serial.println("Running at wire 8. Baud rate 115200");
-    }
+
+    SetupWifi();
+}
 
 void loop() {
 }
